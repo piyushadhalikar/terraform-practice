2a522eb8cdd775768de5c0b00d77dc1cf2e25e17
piyushadhalikar <45139280+piyushadhalikar@users.noreply.github.com>
2026-04-14 10:51:21 -0400

    Update basics.tf

--- basics.tf
+++ basics.tf
@@ -3,6 +3,13 @@
   on linux_amd64
 
 2. tf file syntax
+  vim file.tf
   <block> <parameters>{
           arguments
 }
+
+3. after file created
+  terraform init (terraform environment gets created)
+
+4. to check what script in file will do
+  terraform plan
