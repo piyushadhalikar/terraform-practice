22a804c24e25e1d4be2c10b0451f3191d4c9be54
piyushadhalikar <45139280+piyushadhalikar@users.noreply.github.com>
2026-04-20 07:48:43 -0400

    added tf plan output for all output blocks

--- output-for_each.tf
+++ output-for_each.tf
@@ -5,11 +5,32 @@ output "ec2_public_ip" {
     description = "The public IP address of the EC2 instance"
 }
 
+output "ec2_public_dns" {
+    value = [
+        for instance in aws_instance.my_instance : instance.public_dns
+    ]
+    description = "The public DNS name of the EC2 instance"
+}
+
+output "ec2_private_ip" {
+    value = [
+        for instance in aws_instance.my_instance : instance.private_ip
+    ]
+    description = "The private IP address of the EC2 instance"
+}
 #Output:
 
-1. ip showing for both instance created by for_each
-  Changes to Outputs:
-  + ec2_public_ip = [
+1. for both instance created by for_each
+ Changes to Outputs:
+  + ec2_private_ip = [
+      + (known after apply),
+      + (known after apply),
+    ]
+  + ec2_public_dns = [
+      + (known after apply),
+      + (known after apply),
+    ]
+  + ec2_public_ip  = [
       + (known after apply),
       + (known after apply),
     ]
